c525e0aab6567342448cc1e5db0419db234ce241
Manh Phung Quoc <dev@convertdigital.com.au>
2024-02-22 16:08:55 +0700

    Fix condition of the inject script

--- public/external/index.js
+++ public/external/index.js
@@ -1,9 +1,8 @@
-var globalShopifyValueSolis = window.Shopify || null;
-const data = {
-  themeId: window.Shopify.theme.id,
-  shop: window.Shopify.shop,
-}
-
-if (globalShopifyValueSolis) {
+if (window.Shopify) {
+  const data = {
+    themeId: window.Shopify.theme.id,
+    shop: window.Shopify.shop,
+  }
   document.dispatchEvent(new CustomEvent('GlobalVarValue', {detail: JSON.stringify(data)}));
 }
+
